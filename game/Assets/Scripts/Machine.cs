bf0e9c3e4d95fb1fe7c32f3823f3b31216dd0d40
Diego Martínez García <d.martinezg.2021@alumnos.urjc.es>
2024-02-05 16:22:35 +0100

    Movimiento

---
+++ game/Assets/Scripts/Machine.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Machine : MonoBehaviour, IPickable
+    {
+        public void PickUp(Transform father)
+        {
+            //Lock fathers movement
+            //Open minigame interface
+        }
+
+        public void LetGo()
+        {
+            //!!!
+        }
+    }
+}
